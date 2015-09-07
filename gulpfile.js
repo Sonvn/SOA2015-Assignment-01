756506194f8e5e02161cf591e7c1f19f5789b00e
Sonvn <vungocson94@gmail.com>
2015-09-07 17:26:35 +0700

    done desktop app

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 
 require('./gulp-tasks/add-sample-data/addSampleData.js')(gulp);
+require('./gulp-tasks/build/build.js')(gulp);
 
-gulp.task('default', ['add-sample-data'], function () {
+gulp.task('default', ['add-sample-data', 'build-app-desktop'], function () {
 
 });
