27ef9d79cdd8e1b8694b131c25d019f838310185
Son Vu <vungocson94@gmail.com>
2015-09-08 22:09:18 +0700

    minor fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 
 require('./gulp-tasks/add-sample-data/addSampleData.js')(gulp);
-require('./gulp-tasks/build/build-app.js')(gulp);
 require('./gulp-tasks/build/create-api-doc.js')(gulp);
 
-gulp.task('default', ['add-sample-data', 'build-app-desktop', 'create-api-doc'], function () {
-
-});
+gulp.task('default', ['add-sample-data', 'create-api-doc']);
