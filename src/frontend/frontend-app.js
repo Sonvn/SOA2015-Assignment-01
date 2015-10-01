ff17c27bf885d1efb24fa69dc253af910fea898c
Son Vu <vungocson94@gmail.com>
2015-10-01 19:59:07 +0700

    update

--- src/frontend/frontend-app.js
+++ src/frontend/frontend-app.js
@@ -9,6 +9,7 @@ module.exports = {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
 
+        app.use("/image", express.static(__dirname + "../../../uploads"));
         app.use(express.static(__dirname + "/public"));
         app.use(express.static(__dirname + "/../common/public"));
 
